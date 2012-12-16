3a4330eaa87ee8b766f552fb0f3233cecb59a0c6
borus <artur89b@gmail.com>
2012-12-16 16:10:53 +0100

    init commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+	std::cout << "JEPs classification";
+
+	/*
+	 * 1. Read file with data and load data into decision table. 
+	 * 2. Create reduced decision table.
+	 *
+	 */
+
+	int input;
+	std::cin >> input;
+	return 0;
+}
