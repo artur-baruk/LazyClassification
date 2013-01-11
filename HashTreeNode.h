6bd7539f95097ba453e11f807f3e249935d55ef3
przemk <p.koczkodaj@gmail.com>
2013-01-11 02:45:27 +0100

    Update HashTreeNode.h

--- HashTreeNode.h
+++ HashTreeNode.h
@@ -86,7 +86,7 @@ class HashTreeNode {
 			{
 				if(candidates[i]->attributesEquals(p_subset)) //jezeli kandydat == podzbior
 				{
-					candidates[i]->incrementSupport(tClass-1);
+					candidates[i]->incrementSupport(tClass);
 				}
 			}
 		}
